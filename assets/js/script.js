8d54ad79ab53310672951af08347119aa18cb14b
varun bisht <varun.bisht>
2020-05-30 11:52:12 +0530

    chnages done to optimize comment box

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,23 @@ $(document).ready(function(){
 loadNavigation();
 loadFooter();
 })
+
+var disqusObserver=new IntersectionObserver(function(entries){
+
+  if(entries[0].isIntersecting){
+    var disqus_config = function () {
+      this.page.url = document.location.href;
+      this.page.identifier = document.location.pathname;
+    };
+    (function() {
+    var d = document, s = d.createElement('script');
+    s.src = 'https://techylane.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
+
+    disqusObserver.disconnect();
+  }
+},{threshold:[0]});
+
+disqusObserver.observe(document.querySelector("#disqus_thread"));
