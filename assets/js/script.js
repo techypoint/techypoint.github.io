ff8f5ab74b356c11fc186ec148b88b24e4ca2eb2
sukriti4 <sukriti.sinha007@gmail.com>
2020-07-25 19:50:44 +0530

    Changes for Blog Page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,63 @@
-var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-scrolled");
-    } else {
-      $("#mainNav").removeClass("navbar-scrolled");
+(() => {
+
+  const bindEvents = () => {
+    const headerToggleBar = document.querySelector(".toggle-bar");
+    const toggleContainer = document.querySelector(".toggle-container");
+    if (headerToggleBar && toggleContainer) {
+      headerToggleBar.addEventListener("click", () => {
+        if (toggleContainer.classList.contains("open")) {
+          toggleContainer.classList.remove("open");
+          headerToggleBar.classList.remove("menu-close");
+        } else {
+          toggleContainer.classList.add("open");
+          headerToggleBar.classList.add("menu-close");
+        }
+      });
     }
-  };
-$(window).scroll(navbarCollapse);
 
-$(document).ready(function(){
-navbarCollapse();
-})
-
-if(document.querySelector("#disqus_thread")){
-
-var disqusObserver=new IntersectionObserver(function(entries){
-
-  if(entries[0].isIntersecting){
-    var disqus_config = function () {
-      this.page.url = document.location.href;
-      this.page.identifier = document.location.pathname;
-    };
-    (function() {
-    var d = document, s = d.createElement('script');
-    s.src = 'https://techylane.disqus.com/embed.js';
-    s.setAttribute('data-timestamp', +new Date());
-    (d.head || d.body).appendChild(s);
-    })();
-
-    disqusObserver.disconnect();
+    window.addEventListener('scroll', ()=>{
+      const header = document.querySelector('.header-section');
+      if(window.pageYOffset > header.clientHeight){
+         header.classList.add('sticky-header');
+      }else{
+        header.classList.remove('sticky-header');
+      }
+    });
   }
-},{threshold:[0]});
-disqusObserver.observe(document.querySelector("#disqus_thread"));
 
-}
+  const loadEvents = () => {
+    if (document.querySelector("#disqus_thread")) {
+      var disqusObserver = new IntersectionObserver(
+        function(entries) {
+          if (entries[0].isIntersecting) {
+            var disqus_config = function() {
+              this.page.url = document.location.href;
+              this.page.identifier = document.location.pathname;
+            };
+            (function() {
+              var d = document,
+                s = d.createElement("script");
+              s.src = "https://techylane.disqus.com/embed.js";
+              s.setAttribute("data-timestamp", +new Date());
+              (d.head || d.body).appendChild(s);
+            })();
+    
+            disqusObserver.disconnect();
+          }
+        },
+        { threshold: [0] }
+      );
+      disqusObserver.observe(document.querySelector("#disqus_thread"));
+    }
+  }
+
+  const init = () => {
+    bindEvents();
+    loadEvents();
+  };
+
+  init();
+})();
+
+
+
